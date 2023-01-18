996216383fb2a7d4c985f362ee174e00556dd10e
Karishma Gupta <121055816+Karishma-gupta2023@users.noreply.github.com>
2023-01-18 15:49:33 +0000

    [lay with let,var and const

--- test/one.js
+++ test/one.js
@@ -1,1 +1,12 @@
 console.log("hitrsh");
+const a = "kishu";
+let b = "hitesh";
+var c = 67;
+d= y;
+console.table([a,b,c,d]);
+console.table([typeof(a),typeof(b),typeof(c),typeof(d)]);
+
+b="ccc"
+c=88
+d=9
+console.table([a,typeof(a),b,typeof(b),c,typeof(c),d,typeof(d)]);
